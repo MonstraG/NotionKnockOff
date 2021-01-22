df102c93510122f2ee75abb56c184245ecb48038
MonstraG <ars.garelyshev@yandex.ru>
2021-01-23 00:45:45 +0500

    removed a bit more useless stuff

--- components/Block/Block.tsx
+++ components/Block/Block.tsx
@@ -23,13 +23,11 @@ const Block: FC<Props & Style> = ({ style, onBreak }) => {
   const appliedStyles = isBroken(state) ? undefined : style;
 
   return (
-    <div style={appliedStyles} className={`${styles.container} ${isBroken(state) && styles.broken}`}>
-      <div className={isBroken(state) ? styles.fragmets : styles.block} onPointerDown={breakMore}>
-        <div className={`${styles.part} ${styles.p1}`} style={{ display: crackDisplay(1)(state) }} />
-        <div className={`${styles.part} ${styles.p2}`} style={{ display: crackDisplay(2)(state) }} />
-        <div className={`${styles.part} ${styles.p3}`} style={{ display: crackDisplay(3)(state) }} />
-        <div className={`${styles.part} ${styles.p4}`} style={{ display: crackDisplay(4)(state) }} />
-      </div>
+    <div style={appliedStyles} className={isBroken(state) ? styles.broken : styles.block} onPointerDown={breakMore}>
+      <div className={`${styles.part} ${styles.p1}`} style={{ display: crackDisplay(1)(state) }} />
+      <div className={`${styles.part} ${styles.p2}`} style={{ display: crackDisplay(2)(state) }} />
+      <div className={`${styles.part} ${styles.p3}`} style={{ display: crackDisplay(3)(state) }} />
+      <div className={`${styles.part} ${styles.p4}`} style={{ display: crackDisplay(4)(state) }} />
     </div>
   )
 }
