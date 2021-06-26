3fc190d65f5fe4b622bfc612c7bc0c10af190fc2
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2021-06-26 23:54:12 +0500

    Moved Editor internal styles into editor component

--- components/Editor/MarkdownEditor.tsx
+++ components/Editor/MarkdownEditor.tsx
@@ -1,9 +1,32 @@
 import { FC } from "react";
 import RichMarkdownEditor from "rich-markdown-editor";
 import EditorStore from "~/components/Editor/EditorStore";
+import styled, { css } from "styled-components";
+
+//todo: dark selectors/menus
+
+const CustomRichMarkdownEditor = styled(RichMarkdownEditor)<{ dark?: boolean }>`
+  ${({ dark }) =>
+    dark &&
+    css`
+      .notice-block {
+        background: transparent !important;
+        color: ${({ theme }) => theme.text};
+        &.tip {
+          box-shadow: 0 0 0 1px #9e5cf7 inset;
+        }
+        &.warning {
+          box-shadow: 0 0 0 1px #f5be31 inset;
+        }
+        &.info {
+          box-shadow: 0 0 0 1px #ff5c80 inset;
+        }
+      }
+    `}
+`;
 
 const MarkdownEditor: FC<{ value: string }> = ({ value }) => (
-  <RichMarkdownEditor
+  <CustomRichMarkdownEditor
     defaultValue={value}
     dark
     onSave={(_) => EditorStore.save()}
