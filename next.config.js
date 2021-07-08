03cc0d4327feec8f0e2493a2456521faa4d8b70b
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2021-07-09 00:03:14 +0500

    Simplify aliases

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require("path");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: false
 });
 
 module.exports = withBundleAnalyzer({
+  webpack: (config) => {
+    config.resolve.alias["~"] = path.resolve(__dirname);
+    return config;
+  },
   reactStrictMode: true
 });
